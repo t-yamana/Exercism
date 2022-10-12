7095a7c1902f59581997ad0d53439fc44b96a037
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-10-13 02:23:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Exercism/AssemblyLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+static class AssemblyLine
+{
+  private static readonly int _defaultProduce = 221;
+
+  public static double SuccessRate(int speed)
+  {
+    return speed switch
+    {
+      <=0 => 0.0,
+      <=4 => 1.0,
+      <=8 => 0.9,
+      <=9 => 0.8,
+      _ => 0.77
+    };
+  }
+
+  public static double ProductionRatePerHour(int speed)
+  {
+    return SuccessRate(speed) * _defaultProduce * speed;
+  }
+
+  public static int WorkingItemsPerMinute(int speed)
+  {
+    return (int)(ProductionRatePerHour(speed) / 60);
+  }
+}
+
