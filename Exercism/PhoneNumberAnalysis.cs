8e9dce6d2acf9c68fd3b8a14fc78ef6288be890a
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-10-14 01:31:31 +0900

    tuple1

---
+++ Exercism/PhoneNumberAnalysis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class PhoneNumber
+{
+  private static readonly string NewYorkNum = "212";
+  private static readonly string FakeNum = "555";
+  public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+  {
+    string[] nums = phoneNumber.Split('-');
+    return (nums[0] == NewYorkNum, nums[1] == FakeNum, nums[2]);
+  }
+
+  public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+  {
+    return phoneNumberInfo.IsFake;
+  }
+}
+
