b1b4cbfd566f70ef4914dcf1f592e72ef005ebb5
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-10-18 03:57:10 +0900

    Rename as default program

---
+++ Exercism/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercism
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var test = Badge.Print(3, "abe", "abe");
+
+      // Console.WriteLine("Hello World!");
+      Console.WriteLine(test);
+    }
+  }
+}
+
