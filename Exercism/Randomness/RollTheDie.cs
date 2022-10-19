084f6d360bf54db474818631a5123ecc358f61ae
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-10-20 00:53:56 +0900

    feature: Randomness

---
+++ Exercism/Randomness/RollTheDie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Player
+{
+  private readonly int _dieSize = 18;
+  private readonly double _maxStrength = 100.0;
+
+  private Random rand = new Random();
+
+  public int RollDie() => rand.Next(1, _dieSize + 1);
+  public double GenerateSpellStrength() => rand.NextDouble() * _maxStrength;
+}
+
