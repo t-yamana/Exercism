84f9eebb6ecc80ae077e61cea4811209b5d67db3
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-11-11 01:12:31 +0900

    trained

---
+++ Exercism/Exceptions/CalculatorConundrum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercism.Exceptions
+{
+  public static class SimpleCalculator
+  {
+    public static string Calculate(int operand1, int operand2, string operation)
+    {
+      string answer = $"{operand1} {operation} {operand2} = ";
+      try
+      {
+        int calculated = operation switch
+        {
+          "+" => SimpleOperation.Addition(operand1, operand2),
+          "*" => SimpleOperation.Multiplication(operand1, operand2),
+          "/" => SimpleOperation.Division(operand1, operand2),
+
+          "" => throw new ArgumentException(),
+          null => throw new ArgumentNullException(),
+          _ => throw new ArgumentOutOfRangeException(),
+        };
+        answer += calculated.ToString();
+      }
+      catch (DivideByZeroException e)
+      {
+        answer = "Division by zero is not allowed.";
+      }
+      // other exception goes through
+      return answer;
+    }
+  }
+}
+
