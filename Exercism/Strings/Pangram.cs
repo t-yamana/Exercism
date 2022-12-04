0ef9e6188f65e1d40aaf6880ae37db694eff8e66
MaNyank <46816457+t-yamana@users.noreply.github.com>
2022-12-05 04:28:50 +0900

    trained

---
+++ Exercism/Strings/Pangram.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Exercism.Strings
+{
+  public static class Pangram
+  {
+    public static bool IsPangram(string input)
+    {
+      var chars = new HashSet<char>();
+
+      Regex re = new Regex("[a-z]+");
+      var matches = re.Matches(input.ToLower());
+      foreach (Match m in matches)
+      {
+        chars.UnionWith(m.Value.ToCharArray());
+        
+        // Bad
+        // m.Value.ToCharArray().Select(c => chars.Add(c));
+      }
+
+      return chars.Count == "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Length;
+    }
+  }
+}
+
