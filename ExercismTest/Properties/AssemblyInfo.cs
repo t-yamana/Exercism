77cc2488ea419e5b5a53b028e0f99cabb2ccc3e9
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-01-03 02:56:33 +0900

    TDD用環境

---
+++ ExercismTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExercismTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExercismTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7dfc5c8-d712-4459-834d-70b70351f11a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExercismTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Exercism.Equality;
+using System.Collections.Generic;
+
+namespace ExercismTest
+{
+  [TestClass]
+  public class LandGrabInSpace
+  {
+    [TestMethod]
+    public void Test1()
+    {
+      var ch = new ClaimsHandler();
+      ch.StakeClaim(new Plot(new Coord(1, 1), new Coord(2, 1), new Coord(1, 2), new Coord(2, 2)));
+      var claimed = ch.IsClaimStaked(new Plot(new Coord(1, 1), new Coord(2, 1), new Coord(1, 2), new Coord(2, 2)));
+      Assert.IsTrue(claimed);
+    }
+
+    [TestMethod]
+    public void Test2()
+    {
+      var ch = new ClaimsHandler();
+      ch.StakeClaim(new Plot(new Coord(1, 1), new Coord(2, 1), new Coord(1, 2), new Coord(2, 2)));
+      ch.StakeClaim(new Plot(new Coord(10, 1), new Coord(20, 1), new Coord(10, 2), new Coord(20, 2)));
+      var lastClaim = ch.IsLastClaim(new Plot(new Coord(10, 1), new Coord(20, 1), new Coord(10, 2), new Coord(20, 2)));
+      Assert.IsTrue(lastClaim);
+    }
+
+    [TestMethod]
+    public void Test3()
+    {
+      var ch = new ClaimsHandler();
+      var longer = new Plot(new Coord(10, 1), new Coord(20, 1), new Coord(10, 2), new Coord(20, 2));
+      var shorter = new Plot(new Coord(1, 1), new Coord(2, 1), new Coord(1, 2), new Coord(2, 2));
+      ch.StakeClaim(longer);
+      ch.StakeClaim(shorter);
+      Assert.AreEqual(longer, ch.GetClaimWithLongestSide());
+    }
+  }
+}
+
