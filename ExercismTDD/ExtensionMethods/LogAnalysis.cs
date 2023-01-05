2686fcdcf9ad08dfcb43dfbbaec071165f48ff8e
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-01-06 03:32:13 +0900

    convert category TDD

---
+++ ExercismTDD/ExtensionMethods/LogAnalysis.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace ExercismTDD.ExtensionMethods
+{
+  public static class LogAnalysis
+  {
+    public static string SubstringAfter(this string text, string partial)
+    {
+      // string[0] => char Type
+      string sbst = text.Split(partial[0]).Last();
+      return sbst;
+    }
+
+    public static string SubstringBetween(this string text, string char1, string char2)
+    {
+      string sbst = text.Split(char1[0]).Last().Split(char2[0]).First();
+      return sbst;
+    }
+
+    public static string Message(this string text)
+    {
+      return text.SubstringAfter(": ");
+    }
+
+    public static string LogLevel(this string text)
+    {
+      return text.SubstringBetween("[", "]");
+    }
+  }
+}
+
---
+++ ExercismTest/UnitTest_ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+using ExercismTDD.ExtensionMethods;
+
+namespace ExercismTest.ExtensionMethods
+{
+  [TestClass]
+  public class EnterSomeName
+  {
+    [TestMethod]
+    public void Test1()
+    {
+    }
+  }
+}
+
