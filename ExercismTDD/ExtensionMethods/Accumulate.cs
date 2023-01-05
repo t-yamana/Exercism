f76c77a7917903e6fbe6034dce20140e316620d1
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-01-06 03:32:20 +0900

    trained

---
+++ ExercismTDD/ExtensionMethods/Accumulate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExercismTDD.ExtensionMethods
+{
+  public static class AccumulateExtensions
+  {
+    public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
+    {
+      return collection.Select(func);
+    }
+  }
+}
+
