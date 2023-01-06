c86740d1b7d9e5115350877ac5f3b9c8c9b9590f
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-01-07 04:50:11 +0900

    trained

---
+++ Exercism/Strings/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercism.Strings
+{
+  public static class ReverseString
+  {
+    public static string Reverse(string input) => string.Join("", input.Reverse());
+  }
+}
+
