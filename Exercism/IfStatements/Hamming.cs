0be40ba19a90af56e84a774dffbc01cc8d130fe8
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-01-15 02:45:01 +0900

    feat: trained

---
+++ Exercism/IfStatements/Hamming.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercism.IfStatements
+{
+  public static class Hamming
+  {
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+      if (firstStrand.Length != secondStrand.Length)
+      {
+        throw new ArgumentException();
+      }
+
+      int hamdist = 0;
+      for (int i = 0; i < firstStrand.Length; i++)
+      {
+        hamdist += firstStrand[i] == secondStrand[i] ? 0 : 1;
+      }
+      return hamdist;
+    }
+  }
+}
+
