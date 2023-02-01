a412e3cf3c16bd1b90b9246c3842a031c8d204d6
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-02 00:47:06 +0900

    trained

---
+++ Exercism/Numbers/PerfectNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercism.Numbers
+{
+  public enum Classification
+  {
+    Deficient = -1,
+    Perfect  = 0,
+    Abundant = 1,
+  }
+
+  public static class PerfectNumbers
+  {
+    public static Classification Classify(int number)
+    {
+      if (number <= 0) throw new ArgumentOutOfRangeException();
+
+      var total = Enumerable.Range(1, number / 2)
+        .Where(n => number % n == 0)
+        .Sum().CompareTo(number);
+
+      return (Classification)total;
+    }
+  }
+}
+
