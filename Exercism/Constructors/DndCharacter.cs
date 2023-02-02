c7bd993c54a67711608b385267d162aa2c94ad33
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-03 02:26:48 +0900

    trained

---
+++ Exercism/Constructors/DndCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercism.Constructor
+{
+  public class DndCharacter
+  {
+    static Random rnd = new Random();
+
+    public int Strength { get; }
+    public int Dexterity { get; }
+    public int Constitution { get; }
+    public int Intelligence { get; }
+    public int Wisdom { get; }
+    public int Charisma { get; }
+    public int Hitpoints { get; }
+
+    DndCharacter()
+    {
+      Strength = Ability();
+      Dexterity = Ability();
+      Constitution = Ability();
+      Intelligence = Ability();
+      Wisdom = Ability();
+      Charisma = Ability();
+      Hitpoints = 10 + Modifier(Constitution);
+    }
+
+    public static int Modifier(int score) => (int)Math.Floor((score - 10) / 2.0);
+
+    public static int Ability()
+    {
+      int[] Dies = { RollDice(), RollDice(), RollDice(), RollDice() };
+      return Dies.OrderBy(die => -1 * die).Take(3).Sum();
+    }
+
+    public static DndCharacter Generate()
+    {
+      return new DndCharacter();
+    }
+
+    static int RollDice() => rnd.Next(1, 6 + 1);
+  }
+}
+
