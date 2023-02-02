b7b3bad6ae90489b0103957ac71f7f31d6e7c5fe
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-03 00:27:15 +0900

    trained

---
+++ Exercism/Numbers/CollatzConjecture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercism.Numbers
+{
+  public static class CollatzConjecture
+  {
+    public static int Steps(int number)
+    {
+      if (number <= 0) throw new ArgumentOutOfRangeException();
+
+      int step = 0;
+      while (number != 1)
+      {
+        number = number % 2 == 0 ? (number / 2) : (3 * number + 1);
+        step++;
+      }
+      return step;
+    }
+  }
+}
+
