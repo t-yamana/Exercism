47848bcdcd334b3c2deb30040303301a7e6e0706
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-14 00:52:32 +0900

    trained

---
+++ Exercism/ForLoops/Series.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercism.ForLoops
+{
+  public static class Series
+  {
+    public static string[] Slices(string numbers, int sliceLength)
+    {
+      int patterns = numbers.Count() - sliceLength + 1;
+      if (patterns <= 0 || sliceLength <= 0)
+      {
+        throw new ArgumentException();
+      }
+
+      var answers = new string[patterns];
+      for (int i = 0; i < patterns; i++)
+      {
+        answers[i] = new string(numbers.Take(new Range(i, i + sliceLength)).ToArray());
+      }
+      return answers;
+    }
+  }
+}
+
