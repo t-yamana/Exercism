986ee03f1f9364603aa410058f6e2e276a675f0b
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-25 14:30:20 +0900

    prepare

---
+++ Exercism/Enums/Allergies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercism.Enums
+{
+  public enum Allergen
+  {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats
+  }
+
+  public class Allergies
+  {
+    public Allergies(int mask)
+    {
+    }
+
+    public bool IsAllergicTo(Allergen allergen)
+    {
+      throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public Allergen[] List()
+    {
+      throw new NotImplementedException("You need to implement this function.");
+    }
+  }
+}
+
