de0899b37d2fea12a6433a30e5e41846d5372caf
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-28 22:49:12 +0900

    trained

--- Exercism/Enums/Allergies.cs
+++ Exercism/Enums/Allergies.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercism.Enums
 {
@@ -16,18 +18,27 @@ namespace Exercism.Enums
 
   public class Allergies
   {
+    readonly int _mask;
+
     public Allergies(int mask)
     {
+      _mask = mask;
     }
 
     public bool IsAllergicTo(Allergen allergen)
     {
-      throw new NotImplementedException("You need to implement this function.");
+      int mask = 1 << (int)allergen;
+      return (_mask & mask) > 0;
     }
 
     public Allergen[] List()
     {
-      throw new NotImplementedException("You need to implement this function.");
+      // Enum の配列(IEnumerable)化
+      var arrAllergen = (IEnumerable<Allergen>)Enum.GetValues(typeof(Allergen));
+
+      return arrAllergen
+        .OrderBy(al => (int)al)
+        .Where(al => (1 << (int)al & _mask) > 0).ToArray();
     }
   }
 }
