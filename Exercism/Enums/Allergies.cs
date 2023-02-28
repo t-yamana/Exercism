97001f3a839fa113e4f90cb211e4697103b12320
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-02-28 23:42:59 +0900

    refac

--- Exercism/Enums/Allergies.cs
+++ Exercism/Enums/Allergies.cs
@@ -1,44 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Exercism.Enums
 {
   public enum Allergen
   {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs         = 1<<0,  // 1
+    Peanuts      = 1<<1,  // 2
+    Shellfish    = 1<<2,  // 4
+    Strawberries = 1<<3,  // 8
+    Tomatoes     = 1<<4,  // 16
+    Chocolate    = 1<<5,  // 32
+    Pollen       = 1<<6,  // 64
+    Cats         = 1<<7,  // 128
   }
 
   public class Allergies
   {
-    readonly int _mask;
+    readonly int _score;
 
-    public Allergies(int mask)
-    {
-      _mask = mask;
-    }
+    public Allergies(int mask) => _score = mask;
 
-    public bool IsAllergicTo(Allergen allergen)
-    {
-      int mask = 1 << (int)allergen;
-      return (_mask & mask) > 0;
-    }
+    public bool IsAllergicTo(Allergen allergen) =>
+      // ここの書き方はいろいろある
+      (_score & (int)allergen) == (int)allergen;
 
     public Allergen[] List()
     {
-      // Enum の配列(IEnumerable)化
-      var arrAllergen = (IEnumerable<Allergen>)Enum.GetValues(typeof(Allergen));
+      // Enum の配列取得 しかも IEnumerable
+      return Enum.GetValues(typeof(Allergen)).Cast<Allergen>()
 
-      return arrAllergen
-        .OrderBy(al => (int)al)
-        .Where(al => (1 << (int)al & _mask) > 0).ToArray();
+        .Where(al => IsAllergicTo(al))
+        .ToArray();
+
+      // (IEnumerable<Allergen>)Enum.GetValues(typeof(Allergen));
     }
   }
 }
