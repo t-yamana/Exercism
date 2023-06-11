bee390fba967e6c8236d31c1657ac371753ebbec
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-06-12 00:36:52 +0900

    solved: Isogram

---
+++ Exercism/Strings/Isogram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercism.Strings
+{
+  public static class Isogram
+  {
+    public static bool IsIsogram(string word)
+    {
+      // NOT isAlpha()
+      IEnumerable<Char> chars = word.ToLower().Where(c => Char.IsLetter(c));
+      return chars.Count() == chars.Distinct().Count();
+    }
+  }
+}
+
