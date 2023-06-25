2c01a3fb945487eae7ab041b54d20b53b919263e
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-06-25 23:05:01 +0900

    refac: DifferenceOfSquares

--- Exercism/Numbers/DifferenceOfSquares.cs
+++ Exercism/Numbers/DifferenceOfSquares.cs
@@ -8,33 +8,20 @@ namespace Exercism.Numbers
   {
     public static int CalculateSquareOfSum(int max)
     {
-      int sum = Enumerable.Range(1, max).Aggregate(
-        new List<int> { 0 },
-        (list, n) =>
-        {
-          list.Add(list.Last() + n);
-          return list;
-        }).Last();
-      return sum * sum;
+      int seed = Enumerable.Range(1, max).Sum(x => x);
+      return seed * seed;
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-      int sum = Enumerable.Range(1, max).Aggregate(
-        new List<int> (),  // answer container
-        (list, n) =>
-        {
-          list.Add(n * n);
-          return list;
-        }).Sum();
+      int sum = Enumerable.Range(1, max).Sum(x => x * x);
       return sum;
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-      int a = CalculateSquareOfSum(max);
-      int b = CalculateSumOfSquares(max);
-      return Math.Abs(a - b);
+      // (x + y + z)**2 - (x** + y** + z**) = 2(xy + yz + zx)
+      return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
     }
   }
 }
