ebbfe3d9d6fd8405dbfb805c09221345bc15e104
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-07-17 02:29:27 +0900

    refac: SumOfMutiples

--- Exercism/Numbers/SumOfMultiples.cs
+++ Exercism/Numbers/SumOfMultiples.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Exercism.Numbers
@@ -8,11 +7,9 @@ namespace Exercism.Numbers
   {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-      var pt = multiples
-        .Where(n => 0 < n && n <= max)
-        .Select(n => Enumerable.Range(1, (max-1) / n).Select(m => n * m))
-        .SelectMany(arr => arr)
-        .Distinct()
+      var pt = Enumerable
+        .Range(0, max)
+        .Where(n => multiples.Any(m => 0 < m && n % m == 0))
         .Sum();
 
       return pt;
