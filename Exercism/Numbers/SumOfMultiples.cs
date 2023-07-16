5bddcf62c13feda664d82c351bdb746e1b48720e
MaNyank <46816457+t-yamana@users.noreply.github.com>
2023-07-17 02:09:15 +0900

    Solved: SumOfMultiples

---
+++ Exercism/Numbers/SumOfMultiples.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercism.Numbers
+{
+  public static class SumOfMultiples
+  {
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+      var pt = multiples
+        .Where(n => 0 < n && n <= max)
+        .Select(n => Enumerable.Range(1, (max-1) / n).Select(m => n * m))
+        .SelectMany(arr => arr)
+        .Distinct()
+        .Sum();
+
+      return pt;
+    }
+  }
+}
+
